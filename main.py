836bc51f79615225cff42c8ca80b4223875dab8f
J4n <jandziekan333@gmail.com>
2023-03-19 14:38:48 +0100

    readFile

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 
-print("hello02")
+# wczytanie danych treningowych
+training_set = []
+
+with open("iris_training.txt", 'r') as file:
+    lines = file.readlines()
+
+for line in lines:
+    items = line.strip().replace('\t', '').replace(',', '.').split()
+    if not items[0].isalpha():
+        training_set.append([float(x) for x in items[0:4]])
+
+for row in training_set:
+    print(row)
