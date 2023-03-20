dd275753f39b4ff31c44730af16222bf026e106c
J4n <jandziekan333@gmail.com>
2023-03-20 14:21:40 +0100

    euclideans

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 
-# wczytanie danych treningowych
-training_set = []
+# funckcja zwracająca dystans pomiędzy punktami ze wzrou Eyklidesa;
+def euclideans_distance(x, y):
+    distance = 0.0
+    for i in range(len(x)):
+        distance += (x[i] - y[i])**2
+    return distance**0.5
 
-with open("iris_training.txt", 'r') as file:
-    lines = file.readlines()
+def predict(training_data, test_data, k):
+    disances = []
+    #for i in range(len(training_data)):
 
-for line in lines:
-    items = line.strip().replace('\t', '').replace(',', '.').split()
-    if not items[0].isalpha():
-        training_set.append([float(x) for x in items[0:4]])
 
-for row in training_set:
-    print(row)
+
+# funkcja wczytująca dane z pliku tekstowego
+def load_data(filename):
+    dataset = []
+    with open(filename) as file:
+        for line in file:
+            items = line.strip().replace('\t', '').replace(',', '.').split()
+            if not items[0].isalpha():
+                dataset.append([float(x) for x in items[0:4]])
+    return dataset
+
+# wczytanie danych treningowych i testowych
+training_data = load_data('iris_training.txt')
+test_data = load_data('iris_test.txt')
+
+for i in test_data:
+    print(i)
